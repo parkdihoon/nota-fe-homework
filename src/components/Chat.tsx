243bcb45d848919091cd81e72a9aec16c410b2fe
Rogan <rogan@aimmo.co.kr>
2024-10-29 23:26:35 +0900

    feat: [채팅 목록] 선택된 채팅 표시

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,6 +2,7 @@ import { ChatProps } from '../models/chat.interface';
 import { useChatStore } from '../stores/chat.ts';
 
 export const Chat = ({ chat }: ChatProps) => {
+  const selectedChat = useChatStore(state => state.selectedChat);
   const { setSelectedChat } = useChatStore(state => state.actions);
 
   const onHandleClick = () => {
@@ -10,7 +11,7 @@ export const Chat = ({ chat }: ChatProps) => {
 
   return (
     <>
-      <div className="flex flex-col gap-y-1 m-1 bg-neutral" onClick={onHandleClick}>
+      <div className={`flex flex-col gap-y-1 m-1 ${selectedChat?.id === chat.id ? 'bg-secondary' : 'bg-neutral'}`} onClick={onHandleClick}>
         <span className="self-start">{chat.dialogues?.[0].prompt ?? ''}</span>
         <span className="self-end">{chat.modelName}</span>
       </div>

126383ecbe691239e324ef21cc483572208468e4
Rogan <rogan@aimmo.co.kr>
2024-10-30 22:12:30 +0900

    feat:[현재 채팅] animation 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,18 @@ export default {
         primary: '#00DCF0',
         secondary: '#3164EF',
         neutral: '#252A39',
-      }
+      },
+      keyframes: {
+        bounceDots: {
+          '0%, 80%, 100%': { transform: 'scale(0)' },
+          '40%': { transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        bounceDot1: 'bounceDots 1.2s infinite 0s',
+        bounceDot2: 'bounceDots 1.2s infinite 0.2s',
+        bounceDot3: 'bounceDots 1.2s infinite 0.4s',
+      },
     },
   },
   plugins: [],

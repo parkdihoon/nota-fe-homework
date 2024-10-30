aad0b6de85505924985051aad4a3f3bde3bf9cce
Rogan <rogan@aimmo.co.kr>
2024-10-30 19:30:52 +0900

    feat: react-query 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import './App.css';
 import { ChatList } from './components/ChatList.tsx';
 import { ActivatedChat } from './components/ActivatedChat.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 
 function App() {
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div className="h-full w-full flex flex-row gap-x-1">
         <ChatList />
         <ActivatedChat />
       </div>
-    </>
+    </QueryClientProvider>
   );
 }
 
